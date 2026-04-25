14299c86f2936324f6a3e9e58de35e9b4a25a61c
NabeelaIsham <mijfnabeela@gmail.com>
2026-04-25 20:26:14 +0530

    nabeela

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react({
-    include: '**/*.{jsx,js}',
+    include: ['**/*.jsx', '**/*.js'],
   })],
   build: {
     outDir: 'dist',
