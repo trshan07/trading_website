df18f88158d6ddbaf6622f3ce1ea36c401dfb4e1
NabeelaIsham <mijfnabeela@gmail.com>
2026-04-25 20:16:20 +0530

    nabeela

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js';  // Add .js extension
+import App from './App.jsx';  // Add .js extension
 import reportWebVitals from './reportWebVitals.js'; // Add .js extension
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
