3b470f86d4b32898e195812d7f76669de5af2c64
NabeelaIsham <mijfnabeela@gmail.com>
2026-04-25 19:08:17 +0530

    nabeela

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.',   // ensures Vite looks in the current directory for index.html
   plugins: [react()],
   build: {
     outDir: 'dist',
